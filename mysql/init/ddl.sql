24e3182203e576c74ce8571850005ee674a28256
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-04-13 14:06:53 +0900

    chore: ddl.sqlを追加し，マウントするようにdocker-compose.ymlを修正

---
+++ mysql/init/ddl.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS `database` DEFAULT CHARACTER SET utf8mb4;
+USE `database`;
+
+SET CHARSET utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `database`.`users` (
+  `id` VARCHAR(128) NOT NULL COMMENT 'ユーザID',
+  `name` VARCHAR(128) NOT NULL COMMENT 'ユーザ名',
+  `high_score` INT UNSIGNED NOT NULL COMMENT 'ハイスコア',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+COMMENT = 'ユーザ';
